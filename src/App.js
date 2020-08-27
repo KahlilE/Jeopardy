fd63ae4514b705ba620537d322736c9ccd0135a0
Kahlil Enmond <slope22@hotmail.com>
2020-08-27 13:03:03 -0400

    j start

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contact from './Components/contact/contact'
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Navigation from "./Components/navigation/Navigation";
+import Jeopardy from './Components/jeopardy/Jeopardy';
 
 
 
@@ -12,14 +13,14 @@ class App extends Component {
   render() {
     return (
       <div>
-
+        
         <Navigation />
         <Switch>
 
           <Route path="/welcome/:name" component={Welcome} />
           <Route  path="/clock" component={Clock} />
           <Route path="/contact" component={Contact} />
-
+          <Route path="/jeopardy" component={Jeopardy} />
         </Switch>
       </div>
     );
